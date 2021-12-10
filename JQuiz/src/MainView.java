4f324d4baa04391205af89d7b015301681b21d33
it63070086 <superoof.th@gmail.com>
2021-12-10 16:54:51 +0700

    ToenMangKodTuddek

--- JQuiz/src/MainView.java
+++ JQuiz/src/MainView.java
@@ -1,27 +1,97 @@
-
-import javax.swing.JTable;
-
+import java.awt.*;
+import java.awt.GridBagConstraints;
+import javax.swing.*;
 
 public class MainView extends javax.swing.JFrame {
-
+    private javax.swing.JButton btnEnter = new JButton();
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JPanel jPanel3;
+    private GridBagConstraints gridBagConstraints;
+//    private JPanel jPanel3;
+//    private JLabel jLabel5, jLabel2, jLabel7, jLabel6, jLabel8;
+//    private JButton btnEnter;
     public MainView() {
         initComponents();
         this.setLocationRelativeTo(null);
-        
     }
-    public JTable getTbCourse() {
-        return tbCourse;
+
+    public JButton getBtnCourse() {
+        return btnCourse;
     }
+
+    public JButton getBtnEnter() {
+        return btnEnter;
+    }
+    
+    
+    public void addCoure(String name, int score, String release, String exprie, String owner){
+    btnEnter = new JButton();
+    jLabel2 = new JLabel();
+    jLabel5 = new JLabel();
+    jLabel6 = new JLabel();
+    jLabel7 = new JLabel();
+    jLabel8 = new JLabel();
+    jPanel3 = new JPanel();
+        jPanel3.setLayout(new java.awt.GridBagLayout());
+        jPanel3.setBackground(Color.gray);
+        jLabel5.setText("Course Name : "+name);
+        jPanel3.add(jLabel5, new java.awt.GridBagConstraints());
+        jLabel2.setText("Course Score : "+score);
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 1;
+        jPanel3.add(jLabel2, gridBagConstraints);
+        jLabel7.setText("Course Release : "+release);
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 2;
+        jPanel3.add(jLabel7, gridBagConstraints);
+        jLabel6.setText("Course Expire : "+exprie);
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 3;
+        jPanel3.add(jLabel6, gridBagConstraints);
+
+        jLabel8.setText("Course Owner : "+owner);
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 4;
+        jPanel3.add(jLabel8, gridBagConstraints);
+
+        btnEnter.setText("Enter Course");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 5;
+        gridBagConstraints.ipadx = 7;
+        gridBagConstraints.ipady = 11;
+        gridBagConstraints.insets = new java.awt.Insets(14, 11, 12, 10);
+        jPanel3.add(btnEnter, gridBagConstraints);
+        jPanel11.add(jPanel3);
+    }
+   
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
+        java.awt.GridBagConstraints gridBagConstraints;
 
+        jLabel4 = new javax.swing.JLabel();
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
+        btnCourse = new javax.swing.JButton();
+        jPanel10 = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
-        tbCourse = new javax.swing.JTable();
+        jPanel11 = new javax.swing.JPanel();
+
+        jLabel4.setFont(new java.awt.Font("Segoe UI", 1, 16)); // NOI18N
+        jLabel4.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel4.setText("All Course");
+        jLabel4.setFocusable(false);
+        jLabel4.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -37,27 +107,37 @@ public class MainView extends javax.swing.JFrame {
         jLabel3.setText("Thiraphop Phokied");
         jPanel1.add(jLabel3);
 
-        jButton1.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
-        jButton1.setText("Course");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        btnCourse.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        btnCourse.setText("Course");
+        btnCourse.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                btnCourseActionPerformed(evt);
             }
         });
-        jPanel1.add(jButton1);
+        jPanel1.add(btnCourse);
 
-        tbCourse.setModel(new javax.swing.table.DefaultTableModel(
-            new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
-            },
-            new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
-            }
-        ));
-        jScrollPane1.setViewportView(tbCourse);
+        jScrollPane1.setHorizontalScrollBarPolicy(javax.swing.ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        jScrollPane1.setPreferredSize(new java.awt.Dimension(800, 200));
+
+        jPanel11.setPreferredSize(new java.awt.Dimension(740, 1080));
+        jScrollPane1.setViewportView(jPanel11);
+
+        javax.swing.GroupLayout jPanel10Layout = new javax.swing.GroupLayout(jPanel10);
+        jPanel10.setLayout(jPanel10Layout);
+        jPanel10Layout.setHorizontalGroup(
+            jPanel10Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel10Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 735, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+        jPanel10Layout.setVerticalGroup(
+            jPanel10Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel10Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 390, Short.MAX_VALUE)
+                .addContainerGap())
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -66,24 +146,24 @@ public class MainView extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 820, Short.MAX_VALUE)
+                .addComponent(jPanel10, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 500, Short.MAX_VALUE)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 411, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jPanel10, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void btnCourseActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCourseActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_btnCourseActionPerformed
 
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
@@ -118,11 +198,16 @@ public class MainView extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
+    private javax.swing.JButton btnCourse;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel10;
+    private javax.swing.JPanel jPanel11;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTable tbCourse;
     // End of variables declaration//GEN-END:variables
+
+    void addWindowListener() {
+    }
 }
--- JQuiz/src/QuizController.java
+++ JQuiz/src/QuizController.java
@@ -1,45 +1,62 @@
-import java.awt.*;
+import java.util.*;
 import javax.swing.JOptionPane;
-import net.proteanit.sql.DbUtils;
-//management database table
 import java.sql.*;
 import java.awt.event.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-public class QuizController implements ActionListener, KeyListener{
+public class QuizController implements ActionListener, KeyListener, WindowListener{
+    private ArrayList<Course> course = new ArrayList<Course>();
     private LoginView loginView = new LoginView();
     private RegView regView = new RegView();
     private MainView mainView = new MainView();
+    private CourseModel courseModel = new CourseModel();
     private Connection con = null;
-//    connection
     private ResultSet rs = null;
-//    dataRespone
     private PreparedStatement pst = null;
-//    Command Sql
     public void ShowDataToForm(){
         String selectSql = "SELECT * FROM course";
         try {
             pst = con.prepareStatement(selectSql);
             rs = pst.executeQuery();
-            mainView.getTbCourse().setModel(DbUtils.resultSetToTableModel(rs));
+//            mainView.getTbCourse().setModel(DbUtils.resultSetToTableModel(rs));
         } catch (SQLException ex) {
             Logger.getLogger(QuizController.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
     public QuizController(){
         con = Connect.ConnectDB();
-        ShowDataToForm();
         loginView.setVisible(true);
         loginView.getBtnReg().addActionListener(this);
         regView.getBtnReg().addActionListener(this);
         loginView.getBtnLogin().addActionListener(this);
         regView.getTfEmail().addKeyListener(this);
+        mainView.addWindowListener(this);
+        mainView.getBtnCourse().addActionListener(this);
+        mainView.getBtnEnter().addActionListener(this);
     }
     public static void main(String[] args) {
         new QuizController();
     }
     @Override
     public void actionPerformed(ActionEvent e) {
+            System.out.println(e);
+            System.out.println(e.getSource());
+            System.out.println(e.getActionCommand());
+            System.out.println(e.getClass());
+            System.out.println(e.getID());
+            System.out.println(e.getModifiers());
+            System.out.println(e.getWhen());
+        if (e.getSource().equals(mainView.getBtnEnter())){
+            System.out.println(e);
+            System.out.println(e.getSource());
+            System.out.println(e.getActionCommand());
+            System.out.println(e.getClass());
+            System.out.println(e.getID());
+            System.out.println(e.getModifiers());
+            System.out.println(e.getWhen());
+            
+        }
+        
         if (e.getSource().equals(loginView.getBtnReg())){
             loginView.setVisible(false);
             regView.setVisible(true);
@@ -50,7 +67,7 @@ public class QuizController implements ActionListener, KeyListener{
             String fullname = regView.getTfFirstname().getText()+" "+regView.getTfLastname().getText();
             String email = regView.getTfEmail().getText();
             String findSql = "SELECT userName, userPassword FROM user WHERE userName = '"+username+"' OR userEmail = '"+email+"'";
-            String insertSql = "INSERT INTO user (userName, userPassword, userFullname, UserEmail, userRole) VALUES ('"+username+"', '"+password+"', '"+fullname+"', '"+email+"', 'Student');";
+            String insertSql = "INSERT INTO user (userName, userPassword, userFullname, UserEmail, userRole) VALUES (?, ?, ?, ?, ?);";
             try {
                 pst = con.prepareStatement(findSql);
                 rs = pst.executeQuery();
@@ -58,22 +75,27 @@ public class QuizController implements ActionListener, KeyListener{
                     JOptionPane.showMessageDialog(null, "Already User Or Email", "Duplicate Information", JOptionPane.WARNING_MESSAGE);
                 }
                 //                  Valid input check By toenTeen
-                else if(username == "" || password == "" || fullname == "" || email == ""){
+                else if("".equals(username) || "".equals(password) || "".equals(fullname) || "".equals(email)){
                     JOptionPane.showMessageDialog(null, "Empty Informatin !!", "Empty Information", JOptionPane.WARNING_MESSAGE);
                 }
-                else if(regView.getTfEmail().getText().indexOf("@") == -1){
+                else if(!regView.getTfEmail().getText().contains("@")){
                     JOptionPane.showMessageDialog(null, "Invalid Email Information !!", "Invalid Email", JOptionPane.WARNING_MESSAGE);
                 }
                 else{
-                    //pst = con.prepareStatement(insertSql);
-                    //rs = pst.executeQuery();
+                    pst = con.prepareStatement(insertSql);
+                    pst.setString(1, username);
+                    pst.setString(2, password);
+                    pst.setString(3, fullname);
+                    pst.setString(4, email);
+                    pst.setString(5, "Student");
+                    pst.execute();
+                    loginView.setVisible(true);
+                    regView.setVisible(false);
                 }
                 
             } catch (SQLException ex) {
                 Logger.getLogger(QuizController.class.getName()).log(Level.SEVERE, null, ex);
             }
-            loginView.setVisible(true);
-            regView.setVisible(false);
         }
 //        Login
         if (e.getSource().equals(loginView.getBtnLogin())){
@@ -105,12 +127,40 @@ public class QuizController implements ActionListener, KeyListener{
 
     @Override
     public void keyReleased(KeyEvent ke) {
-        if(regView.getTfEmail().getText().indexOf("@") == -1){
+        if(!regView.getTfEmail().getText().contains("@")){
             regView.getLbEmail().setText("Fuck Oof");
         }
         else{
             regView.getLbEmail().setText("");
         }
     }
-    
+
+    @Override
+    public void windowOpened(WindowEvent we) {
+        courseModel.load();
+        course = courseModel.getCourse();
+        for (int i=0; i<course.size(); i++){
+            mainView.addCoure(course.get(i).getCourseName(), course.get(i).getCourseScore(), course.get(i).getCourseRelease(), course.get(i).getCourseExpire(), course.get(i).getCourseOwner());
+        }
+        System.out.println("test");
+        mainView.setVisible(true);
+    }
+    @Override
+    public void windowClosing(WindowEvent we) {
+    }
+    @Override
+    public void windowClosed(WindowEvent we) {
+    }
+    @Override
+    public void windowIconified(WindowEvent we) {
+    }
+    @Override
+    public void windowDeiconified(WindowEvent we) {
+    }
+    @Override
+    public void windowActivated(WindowEvent we) {
+    }
+    @Override
+    public void windowDeactivated(WindowEvent we) {
+    }
 }
