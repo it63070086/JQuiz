d2317d7b81fe9595f9db1488f98ed318133a20a3
Noppanut <toen_teen@hotmail.com>
2021-12-12 16:32:32 +0700

    Lastest Version

---
+++ src/Course.java
@@ -0,0 +1,97 @@
+
+import java.util.ArrayList;
+import javax.swing.JTextField;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author pc
+ */
+public class Course {
+    private int CourseID;
+    private String CourseName;
+    private int CourseScore;
+    private String CourseRelease;
+    private String CourseExpire;
+    private String CourseOwner;
+    private ArrayList<Quiz> Quiz;
+    
+    public Course(){
+        
+    }
+    public Course(int CourseID, String CourseName, int CourseScore, String CourseRelease, String CourseExpire, String CourseOwner, ArrayList<Quiz> Quiz){
+        setCourseID(CourseID);
+        setCourseName(CourseName);
+        setCourseScore(CourseScore);
+        setCourseRelease(CourseRelease);
+        setCourseExpire(CourseExpire);
+        setCourseOwner(CourseOwner);
+        setQuiz(Quiz);
+    }
+
+    Course(int size, String text, int parseInt, String text0, JTextField tfCourseExpire, ArrayList<Quiz> quiz) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public int getCourseID() {
+        return CourseID;
+    }
+
+    public void setCourseID(int CourseID) {
+        this.CourseID = CourseID;
+    }
+
+    public String getCourseName() {
+        return CourseName;
+    }
+
+    public void setCourseName(String CourseName) {
+        this.CourseName = CourseName;
+    }
+
+    public int getCourseScore() {
+        return CourseScore;
+    }
+
+    public void setCourseScore(int CourseScore) {
+        this.CourseScore = CourseScore;
+    }
+
+    public String getCourseRelease() {
+        return CourseRelease;
+    }
+
+    public void setCourseRelease(String CourseRelease) {
+        this.CourseRelease = CourseRelease;
+    }
+
+    public String getCourseExpire() {
+        return CourseExpire;
+    }
+
+    public void setCourseExpire(String CourseExpire) {
+        this.CourseExpire = CourseExpire;
+    }
+
+    public String getCourseOwner() {
+        return CourseOwner;
+    }
+
+    public void setCourseOwner(String CourseOwner) {
+        this.CourseOwner = CourseOwner;
+    }
+
+    public ArrayList<Quiz> getQuiz() {
+        return Quiz;
+    }
+
+    public void setQuiz(ArrayList<Quiz> Quiz) {
+        this.Quiz = Quiz;
+    }
+    
+}
