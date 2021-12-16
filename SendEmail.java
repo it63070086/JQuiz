9eb8cdc3f084995155d6075104fc78b507d5a560
Noppanut <toen_teen@hotmail.com>
2021-12-16 13:47:08 +0700

    send

--- SendEmail.java
+++ SendEmail.java
@@ -1,50 +1,37 @@
-import java.io.UnsupportedEncodingException;
 import java.util.Properties;
-
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
-import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
-
-
 public class SendEmail {
-
-    public static void main(String[] args) {
-
+    public void sendEmail(String emailUser, String passwordUser){
         final String username = "openit.th@gmail.com";
         final String password = "openit1200";
-
         Properties props = new Properties();
         props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.host", "smtp.gmail.com");
         props.put("mail.smtp.port", "587");
-
         Session session = Session.getInstance(props,
-          new javax.mail.Authenticator() {
+                new javax.mail.Authenticator() {
+            @Override
             protected PasswordAuthentication getPasswordAuthentication() {
                 return new PasswordAuthentication(username, password);
             }
-          });
-
+        });
         try {
-
             Message message = new MimeMessage(session);
             message.setFrom(new InternetAddress("openit.th@gmail.com"));
             message.setRecipients(Message.RecipientType.TO,
-                InternetAddress.parse("63070023@kmitl.ac.th"));
-            message.setSubject("Testing Subject");
-            message.setText("Dear Mail tong,"
-                + "\n\n ตุ๊ดเด็ก");
-
+                    InternetAddress.parse(emailUser));
+            message.setSubject("Super Secret Password in Jquiz by Super Mega Ultra Apacha Ultimate Omega Fire Renger Master Group");
+            message.setText("Your Password in JQuiz is "+passwordUser);
             Transport.send(message);
 
             System.out.println("Done");
-
         } catch (MessagingException e) {
             throw new RuntimeException(e);
         }
